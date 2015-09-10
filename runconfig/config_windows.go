22a13abf30d9e38876c1660945a8551af17a9153
John Howard <jhoward@microsoft.com>
2015-09-09 19:23:06 -0700

    Windows: Add volume support
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- runconfig/config_windows.go
+++ runconfig/config_windows.go
@@ -1,5 +1,11 @@
 package runconfig
 
+import (
+	"fmt"
+
+	"github.com/docker/docker/volume"
+)
+
 // ContainerConfigWrapper is a Config wrapper that hold the container Config (portable)
 // and the corresponding HostConfig (non-portable).
 type ContainerConfigWrapper struct {
@@ -10,4 +16,44 @@ type ContainerConfigWrapper struct {
 // getHostConfig gets the HostConfig of the Config.
 func (w *ContainerConfigWrapper) getHostConfig() *HostConfig {
 	return w.HostConfig
+}
+
+// processVolumesAndBindSettings processes the volumes and bind settings
+// which are received from the caller (docker CLI or REST API) in a platform
+// specific manner.
+//
+// This is necessary due to platform specifics, where the spec supplied
+// cannot be parsed accurately by the client as it doesn't know the daemon
+// platform where the spec is relevant.
+//
+// To ensure backwards compatibility of the REST API, the docker CLI passes
+// everything supplied by the user in the config.CLIVolumeSpecs field (used by
+// CLI). However, any pre-existing REST API caller can continue to
+// do as it did before by passing information in through either config.Volumes
+// or HostConfig.Binds.
+func processVolumesAndBindSettings(c *Config, hc *HostConfig) error {
+	// Move everything from the backwards compatibility structure into volumes.
+	// We don't need to worry about potentially overwriting anything as previous
+	// to us being called, we are guaranteed that if CLIVolumeSpecs is populated,
+	// then Volumes not populated. However, don't overwrite if Volumes is
+	// populated as that could be from a REST API caller.
+	if len(c.Volumes) == 0 {
+		c.Volumes = c.CLIVolumeSpecs
+	}
+	c.CLIVolumeSpecs = nil
+
+	// Move bind mounts from config.Volumes into hc.Binds as we do not want
+	// them committed to image configs.
+	for bind := range c.Volumes {
+		mp, err := volume.ParseMountSpec(bind, hc.VolumeDriver)
+		if err != nil {
+			return fmt.Errorf("Unrecognised volume spec: %v", err)
+		}
+		if len(mp.Source) > 0 {
+			hc.Binds = append(hc.Binds, bind)
+			delete(c.Volumes, bind)
+		}
+	}
+
+	return nil
 }
--- volume/drivers/extpoint_test.go
+++ volume/drivers/extpoint_test.go
@@ -1,8 +1,10 @@
 package volumedrivers
 
 import (
+	"runtime"
 	"testing"
 
+	"github.com/docker/docker/volume"
 	"github.com/docker/docker/volume/testutils"
 )
 
@@ -11,7 +13,6 @@ func TestGetDriver(t *testing.T) {
 	if err == nil {
 		t.Fatal("Expected error, was nil")
 	}
-
 	Register(volumetestutils.FakeDriver{}, "fake")
 	d, err := GetDriver("fake")
 	if err != nil {
@@ -19,5 +20,83 @@ func TestGetDriver(t *testing.T) {
 	}
 	if d.Name() != "fake" {
 		t.Fatalf("Expected fake driver, got %s\n", d.Name())
+	}
+}
+
+// testParseMountSpec is a structure used by TestParseMountSpecSplit for
+// specifying test cases for the ParseMountSpec() function.
+type testParseMountSpec struct {
+	bind      string
+	driver    string
+	expDest   string
+	expSource string
+	expName   string
+	expDriver string
+	expRW     bool
+	fail      bool
+}
+
+func TestParseMountSpecSplit(t *testing.T) {
+	var cases []testParseMountSpec
+	if runtime.GOOS == "windows" {
+		cases = []testParseMountSpec{
+			{`c:\:d:`, "local", `d:`, `c:\`, ``, "", true, false},
+			{`c:\:d:\`, "local", `d:\`, `c:\`, ``, "", true, false},
+			// TODO Windows post TP4 - Add readonly support {`c:\:d:\:ro`, "local", `d:\`, `c:\`, ``, "", false, false},
+			{`c:\:d:\:rw`, "local", `d:\`, `c:\`, ``, "", true, false},
+			{`c:\:d:\:foo`, "local", `d:\`, `c:\`, ``, "", false, true},
+			{`name:d::rw`, "local", `d:`, ``, `name`, "local", true, false},
+			{`name:d:`, "local", `d:`, ``, `name`, "local", true, false},
+			// TODO Windows post TP4 - Add readonly support {`name:d::ro`, "local", `d:`, ``, `name`, "local", false, false},
+			{`name:c:`, "", ``, ``, ``, "", true, true},
+			{`driver/name:c:`, "", ``, ``, ``, "", true, true},
+		}
+	} else {
+		cases = []testParseMountSpec{
+			{"/tmp:/tmp1", "", "/tmp1", "/tmp", "", "", true, false},
+			{"/tmp:/tmp2:ro", "", "/tmp2", "/tmp", "", "", false, false},
+			{"/tmp:/tmp3:rw", "", "/tmp3", "/tmp", "", "", true, false},
+			{"/tmp:/tmp4:foo", "", "", "", "", "", false, true},
+			{"name:/named1", "", "/named1", "", "name", "local", true, false},
+			{"name:/named2", "external", "/named2", "", "name", "external", true, false},
+			{"name:/named3:ro", "local", "/named3", "", "name", "local", false, false},
+			{"local/name:/tmp:rw", "", "/tmp", "", "local/name", "local", true, false},
+			{"/tmp:tmp", "", "", "", "", "", true, true},
+		}
+	}
+
+	for _, c := range cases {
+		m, err := volume.ParseMountSpec(c.bind, c.driver)
+		if c.fail {
+			if err == nil {
+				t.Fatalf("Expected error, was nil, for spec %s\n", c.bind)
+			}
+			continue
+		}
+
+		if m == nil || err != nil {
+			t.Fatalf("ParseMountSpec failed for spec %s driver %s error %v\n", c.bind, c.driver, err.Error())
+			continue
+		}
+
+		if m.Destination != c.expDest {
+			t.Fatalf("Expected destination %s, was %s, for spec %s\n", c.expDest, m.Destination, c.bind)
+		}
+
+		if m.Source != c.expSource {
+			t.Fatalf("Expected source %s, was %s, for spec %s\n", c.expSource, m.Source, c.bind)
+		}
+
+		if m.Name != c.expName {
+			t.Fatalf("Expected name %s, was %s for spec %s\n", c.expName, m.Name, c.bind)
+		}
+
+		if m.Driver != c.expDriver {
+			t.Fatalf("Expected driver %s, was %s, for spec %s\n", c.expDriver, m.Driver, c.bind)
+		}
+
+		if m.RW != c.expRW {
+			t.Fatalf("Expected RW %v, was %v for spec %s\n", c.expRW, m.RW, c.bind)
+		}
 	}
 }
