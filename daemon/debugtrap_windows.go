3e6a702b4c159cea2acea2b830429c2b9bad53ed
John Howard <jhoward@microsoft.com>
2016-10-12 15:29:47 -0700

    Spew debugging
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- daemon/debugtrap_windows.go
+++ daemon/debugtrap_windows.go
@@ -6,11 +6,12 @@ import (
 	"syscall"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/docker/docker/pkg/signal"
 	"github.com/docker/docker/pkg/system"
 )
 
-func setupDumpStackTrap(root string) {
+func (d *Daemon) setupDumpStackTrap(root string) {
 	// Windows does not support signals like *nix systems. So instead of
 	// trapping on SIGUSR1 to dump stacks, we wait on a Win32 event to be
 	// signaled.
@@ -24,7 +25,19 @@ func setupDumpStackTrap(root string) {
 			for {
 				syscall.WaitForSingleObject(h, syscall.INFINITE)
 				signal.DumpStacks(root)
+				d.dumpDaemon(root)
 			}
 		}
 	}()
+}
+
+// dumpDaemon is a helper function to dump the daemon data-structures
+// for debugging purposes.
+func (d *Daemon) dumpDaemon(root string) {
+	// Ensure we recover from a panic as we are doing this without any locking
+	defer func() {
+		if r := recover(); r != nil {
+		}
+	}()
+	spew.Dump(d)
 }
