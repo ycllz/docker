26dddc7eb3931904bb47e0b2bdbcfafb30dac5df
Akash Gupta <akagup@microsoft.com>
2017-06-20 14:09:26 -0700

    Added utility package for continuity to support unit tests
    Signed-off-by: Akash Gupta <akagup@microsoft.com>

---
+++ pkg/rootfs/utils.go
@@ -0,0 +1,60 @@
+package rootfs
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"sort"
+)
+
+// Utility functions for rootfs operations that aren't provided by the
+// continuity interface
+
+// These the same as the ioutil package.
+func ReadFile(r RootFS, filename string) ([]byte, error) {
+	f, err := r.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+
+}
+
+func WriteFile(r RootFS, filename string, data []byte, perm os.FileMode) error {
+	f, err := r.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, perm)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	n, err := f.Write(data)
+	if err != nil {
+		return err
+	} else if n != len(data) {
+		return io.ErrShortWrite
+	}
+
+	return nil
+}
+
+func ReadDir(r RootFS, dirname string) ([]os.FileInfo, error) {
+	f, err := r.Open(dirname)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	dirs, err := f.Readdir(-1)
+	if err != nil {
+		return nil, nil
+	}
+	sort.Slice(dirs, func(i, j int) bool { return dirs[i].Name() < dirs[j].Name() })
+	return dirs, nil
+}
